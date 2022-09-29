ff655f9ae5ec3143b669dea031242fae3bfff69b
waly2020 <walyguema@gmail.com>
2022-09-29 17:46:04 +0100

    end

---
+++ public/js/animations.js
@@ -0,0 +1,7 @@
+// let btns_active_article = document.querySelectorAll(".active-article");
+// let detail_articles = document.querySelectorAll(".detail-article");
+// btns_active_article.forEach((btn, index, parent) => {
+//     btn.addEventListener("click", () => {
+//         detail_articles[index].classList.toggle("active");
+//     })
+// })
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const heur = require("./heurs.js");
-
+const ejs = require("ejs");
 const server = express();
-server.use(express.static("public"));
+const mysql = require("mysql");
+const myconnection = require("express-myconnection");
 
-server.get("/", (req, rep) => {
-    rep.sendFile("pages/index.html", { root: __dirname });
-    console.log("page d'accueil charger a " + heur.getHour());
-})
+const options_db = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'people_db'
+}
+
+server.use(myconnection(mysql, options_db, 'pool'));
+
+server.use(express.static("public"));
+server.set('view engine', 'ejs');
+server.set("views", 'public');
+
+heur.getDatas(server,'/','index');
+
+heur.getDatas(server,'/boutique','boutique');
 
 server.listen(3001, () => {
     console.log('port 3001 ouvert');
