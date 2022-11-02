a5dd425ade662d7dd172fc68019a0b49127a1624
waly2020 <walyguema@gmail.com>
2022-11-02 11:09:35 +0100

    end

--- scripts/pool.js
+++ scripts/pool.js
@@ -1,29 +1,23 @@
 const util = require("util");
-const mysql = require("mysql");
+const {Client} = require("pg");
 
-// const pool = mysql.createPool({
-//     user : "waly",
-//     database : "waly_mont_bouet_gabon",
-//     host : "mysql-waly.alwaysdata.net",
-//     password : "walyledev2022",
-//     connectionLimit : 10
-// });
-const pool = mysql.createPool({
+const pool = new Client({
     user : "waly",
     database : "marche-mont-bouet-gabon",
-    host : "postgres://waly:JMJN91hoZgeFx1Kd6FrA6luDjbB6eGiT@dpg-cdh2v5qen0hl21kptpug-a.oregon-postgres.render.com/montbouet",
+    host : "postgres://waly:JMJN91hoZgeFx1Kd6FrA6luDjbB6eGiT@dpg-cdh2v5qen0hl21kptpug-a/montbouet",
     port : 5432,
     password : "JMJN91hoZgeFx1Kd6FrA6luDjbB6eGiT",
-    connectionLimit : 10
 });
 
-pool.getConnection((err,connexion)=>{
-    if(err) throw err;
-    if(connexion){
-        console.log("connecter a la bdd");
-        connexion.release();
-    }
-})
+// pool.getConnection((err,connexion)=>{
+//     if(err) throw err;
+//     if(connexion){
+//         console.log("connecter a la bdd");
+//         connexion.release();
+//     }
+// })
+
+pool.connect();
 
 
 pool.query = util.promisify(pool.query);
